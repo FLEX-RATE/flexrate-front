40bab5deb9be7c5612c0544e46006cd4d780f9f4
yyc0026 <youngchan206@gmail.com>
2025-06-09 18:39:52 +0900

    feat: 지문인증 작업 개별 브랜치

--- src/app/fido2/register/page.tsx
+++ src/app/fido2/register/page.tsx
@@ -1,13 +1,13 @@
-// // app/fido2/register/page.tsx
-// import React from 'react';
+// app/fido2/register/page.tsx
+import React from 'react';
 
-// import PasskeyRegisterForm from '@/components/fido2/PasskeyRegisterForm';
+import PasskeyRegisterForm from '@/components/fido2/PasskeyRegisterForm';
 
-// export default function PasskeyRegisterPage() {
-//   return (
-//     <main>
-//       <h1>패스키 등록</h1>
-//       <PasskeyRegisterForm />
-//     </main>
-//   );
-// }
+export default function PasskeyRegisterPage() {
+  return (
+    <main>
+      <h1>패스키 등록</h1>
+      <PasskeyRegisterForm />
+    </main>
+  );
+}
